f50cd775d90416a458fdc1ce3d824f7c6e534485
Amir <a.yaghoobi.dev@gmail.com>
2019-04-21 14:33:15 +0430

    initial commit

---
+++ examples/simpleOperations.js
@@ -0,0 +1,39 @@
+const Statsly = require('../index')
+
+
+const statsly = new Statsly({
+  port: 6379,
+  host: '127.0.0.1',
+  family: 4,
+  password: '',
+  db: 0
+}, {
+  host: 'localhost',
+  port: 8086,
+})
+
+
+const categories = ['cat01', 'cat02', 'cat03', 'cat04']
+const statsKeys = ['key01', 'key02', 'key03', 'key04', 'key05']
+
+statsly.register(...categories)
+
+// Unregistering "cat04" will prevent inserting that stats into influx
+statsly.unregister('cat04')
+
+// reset stats in redis
+statsly.reset('cat04')
+
+
+statsly.set('cat04', 'key05', 100)
+
+// Generate random stats every 100ms
+setInterval(() => {
+  const c = categories[Math.ceil(Math.random() * categories.length) - 1]
+  const k = statsKeys[Math.ceil(Math.random() * statsKeys.length) - 1]
+  const v = Math.ceil(Math.random() * k.length)
+  statsly.increment(c, k, v)
+}, 100)
+
+// Collect stats and insert them into influx every 10sec
+statsly.startCollector(10000)
